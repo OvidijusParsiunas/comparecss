56e53af28b1aa9abc37a532da5b757849f66aa3d
OvidijusParsiunas <oparsiunas@googlemail.com>
2020-09-17 22:12:28 +0000

    fixed build and lint error

--- src/services/markupManager.ts
+++ src/services/markupManager.ts
@@ -3,21 +3,21 @@ import { ContentMarkupInterface } from '../interfaces/ContentMarkupInterface';
 import Buttons from '../spec/buttons';
 
 export default new class {
-  static retrieveContentMarkup(clickedButtonName: string): ContentMarkupInterface {
-    switch (clickedButtonName) {
-      case 'Badges':
-        return Badges;
-      case 'Buttons':
-        return Buttons;
-      default: {
-        return {
-          bootstrap: '',
-          materialize: '',
-          uikit: '',
-          foundation: '',
-          bulma: '',
-        };
-      }
-    }
-  }
+   retrieveContentMarkup = (clickedButtonName: string): ContentMarkupInterface => {
+     switch (clickedButtonName) {
+       case 'Badges':
+         return Badges;
+       case 'Buttons':
+         return Buttons;
+       default: {
+         return {
+           bootstrap: '',
+           materialize: '',
+           uikit: '',
+           foundation: '',
+           bulma: '',
+         };
+       }
+     }
+   }
 }();
