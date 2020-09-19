0f69f2d67f30779dad2400311c7bc0abee5c091e
OvidijusParsiunas <oparsiunas@googlemail.com>
2020-09-19 13:14:01 +0000

    cleanup

--- src/services/markupManager.ts
+++ src/services/markupManager.ts
@@ -1,23 +1,23 @@
-import Badges from '@/spec/badges';
 import { ContentMarkupInterface } from '../interfaces/ContentMarkupInterface';
+import Badges from '@/spec/badges';
 import Buttons from '../spec/buttons';
 
 export default class MarkupManager {
-   static retrieveContentMarkup(clickedButtonName: string): ContentMarkupInterface {
-     switch (clickedButtonName) {
-       case 'Badges':
-         return Badges;
-       case 'Buttons':
-         return Buttons;
-       default: {
-         return {
-           bootstrap: '',
-           materialize: '',
-           uikit: '',
-           foundation: '',
-           bulma: '',
-         };
-       }
-     }
-   }
+  static retrieveContentMarkup(clickedButtonName: string): ContentMarkupInterface {
+    switch (clickedButtonName) {
+      case 'Badges':
+        return Badges;
+      case 'Buttons':
+        return Buttons;
+      default: {
+        return {
+          bootstrap: '',
+          materialize: '',
+          uikit: '',
+          foundation: '',
+          bulma: '',
+        };
+      }
+    }
+  }
 }
