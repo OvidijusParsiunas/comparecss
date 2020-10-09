1013a698d1254112be583a073019b222e1e5ea67
OvidijusParsiunas <oparsiunas@googlemail.com>
2020-10-09 01:09:49 +0000

    pure form

--- src/cssFrameworks/componentMarkup/forms.ts
+++ src/cssFrameworks/componentMarkup/forms.ts
@@ -461,40 +461,56 @@ export default {
     </div>
   `,
   pure: `
-  <form class="pure-form pure-form-stacked">
-  <fieldset>
-      <legend>Legend</legend>
-      <div class="pure-g">
-          <div class="pure-u-1 pure-u-md-1-3">
-              <label for="multi-first-name">First Name</label>
-              <input type="text" id="multi-first-name" class="pure-u-23-24" />
-          </div>
-          <div class="pure-u-1 pure-u-md-1-3">
+    <div style="width: 50%; display: inline-block">
+      <form class="pure-form pure-form-stacked">
+        <fieldset>
+          <div class="pure-g">
+            <div class="pure-u-1 pure-u-md-1-2">
+              <label for="multi-first-name">Username</label>
+              <input type="text" id="multi-first-name" placeholder="Username" class="pure-u-23-24" />
+            </div>
+            <div class="pure-u-1 pure-u-md-1-2">
               <label for="multi-last-name">Last Name</label>
-              <input type="text" id="multi-last-name" class="pure-u-23-24" />
-          </div>
-          <div class="pure-u-1 pure-u-md-1-3">
-              <label for="multi-email">E-Mail</label>
-              <input type="email" id="multi-email" class="pure-u-23-24" required="" />
-          </div>
-          <div class="pure-u-1 pure-u-md-1-3">
-              <label for="multi-city">City</label>
-              <input type="text" id="multi-city" class="pure-u-23-24" />
-          </div>
-          <div class="pure-u-1 pure-u-md-1-3">
-              <label for="multi-state">State</label>
-              <select id="multi-state" class="pure-input-1-2">
-                  <option>AL</option>
-                  <option>CA</option>
-                  <option>IL</option>
+              <input type="password" placeholder="Password" class="pure-u-23-24" />
+            </div>
+            <div style="margin-top: 15px" class="pure-u-1 pure-u-md-1-2">
+              <label for="multi-state">Continent</label>
+              <select id="multi-state" class="pure-u-23-24">
+                <option>Europe</option>
+                <option>North America</option>
+                <option>South America</option>
+                <option>Asia</option>
+                <option>Australia</option>
+                <option>Africa</option>
+                <option>Antarctica</option>
               </select>
+            </div>
+            <div style="margin-top: 15px" class="pure-u-1 pure-u-md-1-2">
+              <label for="multi-last-name">Disabled input</label>
+              <input type="text" placeholder="Disabled input" disabled="" class="pure-u-23-24" />
+            </div>
+            <div style="margin-top: 15px" class="pure-u-1 pure-u-md-1">
+              <label for="multi-last-name">Comments</label>
+              <textarea class="pure-input-1" placeholder="Comments"></textarea>
+            </div>
+            <div style="margin-top: 15px" class="pure-u-1 pure-u-md-1-2">
+              <label for="checkbox-radio-option-one" class="pure-checkbox">
+              <input type="checkbox" id="checkbox-radio-option-one" value="" />Save information</label>
+            </div>
+            <div style="margin-top: 15px" class="pure-u-1 pure-u-md-1-2">
+              <div style="display: inline-flex">
+                <label for="checkbox-radio-option-two" class="pure-radio">
+                <input type="radio" id="checkbox-radio-option-two" name="optionsRadios" value="option1" checked="" />1</label>
+                <label style="margin-left: 15px" for="checkbox-radio-option-three" class="pure-radio">
+                <input type="radio" id="checkbox-radio-option-three" name="optionsRadios" value="option2" />2</label>
+                <label style="margin-left: 15px" for="checkbox-radio-option-four" class="pure-radio">
+                <input type="radio" id="checkbox-radio-option-four" name="optionsRadios" value="option3" />3</label>
+              </div>
+            </div>
           </div>
-      </div>
-      <label for="multi-terms" class="pure-checkbox">
-          <input type="checkbox" id="multi-terms" /> I&#x27;ve read the terms and conditions</label>
-      <button type="submit" class="pure-button pure-button-primary">Submit</button>
-  </fieldset>
-</form>
+        </fieldset>
+      </form>
+    </div>
     `,
   skeleton: `
   <form>
